9a4578c5919a7f1accd5c499960fad28e5227c36
Your Name <NRankapole@dvtsoftware.com>
2026-03-21 22:09:42 +0200

    feat: Add API function to get user repo information

--- src/app/lib/github.ts
+++ src/app/lib/github.ts
@@ -1,24 +1,51 @@
 import { GithubUser } from "./types/GithubUser";
-import { GithubRepo } from "./types/GithubRepo";
-import { GithubEvent } from "./types/GithubEvent";
+import { GithubRepo, GithubRepoResponse } from "./types/GithubRepo";
 
-export async function getGithubUser (username : string): Promise<GithubUser> {
+
+
+export async function getGithubUser(username: string): Promise<GithubUser> {
     const res = await fetch(`https://api.github.com/users/${username}`);
 
-     if (!res.ok) {
+    if (!res.ok) {
         throw new Error("Failed to get user")
-     }
+    }
 
-     const data = await res.json();
+    const data = await res.json();
 
     return {
-    username: data.login,
-    name: data.name,
-    avatar: data.avatar_url,
-    bio: data.bio,
-    followers: data.followers,
-    following: data.following,
-    publicRepos: data.public_repos,
-  };
+        username: data.login,
+        name: data.name,
+        avatar: data.avatar_url,
+        bio: data.bio,
+        followers: data.followers,
+        following: data.following,
+        publicRepos: data.public_repos
+    };
 }
+
+
+export async function getGithubUserRepos(username: string): Promise<GithubRepo[]> {
+    const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+    if (!res.ok) {
+        throw new Error("Failed to get  repos")
+    }
+
+    const data: GithubRepoResponse[] = await res.json();
+
+    return data.map((repo) => (
+        {
+            name: repo.name,
+            description: repo.description,
+            starCount: repo.stargazers_count,
+            language: repo.language,
+            lastUpdated: repo.updated_at,
+        }
+    ))
+
+}
+
+
+
+
 
