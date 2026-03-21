d2f984776a6ac8e498a605f5bb4baba9fff1c953
Your Name <NRankapole@dvtsoftware.com>
2026-03-21 11:52:05 +0200

    style(login page): Remove form elements and replace with single button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import LoginForm from "./components/login-form";
 
 export default function Page() {
     return (
-        <main>
-            <h1 className="text-2xl text-center">Github personal Dashboard</h1>
+        <main className="pt-10">
+            <h1 className="text-2xl text-center">
+                Github Personal Dashboard
+            </h1>
+            
             <LoginForm />
         </main>
     )
