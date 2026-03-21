451149c25ba47cbf09932ca46b98dc2849ed3eca
Your Name <NRankapole@dvtsoftware.com>
2026-03-21 19:35:27 +0200

    feat: Add event and repo type definition

---
+++ src/app/lib/types/GithubEvent.ts
@@ -0,0 +1,53 @@
+interface GithubBaseEvent {
+    id: string,
+    type: string,
+    createdAt: string,
+    repo: {
+        name: string
+    }
+}
+
+interface WatchEvent extends GithubBaseEvent {
+    type: "WatchEvent",
+    payload: {
+        action: "started",
+    }
+}
+
+interface Commit {
+    sha: string,
+    message: string
+}
+
+interface PushEvent extends GithubBaseEvent {
+    type: "PushEvent",
+    payload: {
+        ref: string,
+        commits: Commit[],
+    }
+}
+
+interface PullRequestEvent extends GithubBaseEvent {
+    type: "PullRequestEvent",
+    payload: {
+        action: "opened" | "closed" | "reopened",
+        pullRequest: {
+            title: string,
+            htmlUrl: string,
+        };
+    }
+}
+
+interface IssuesEvent extends GithubBaseEvent {
+    type : "IssuesEvent",
+    payload : {
+        action : "opened" | "closed" | "reopened" | "edited",
+        issue : {
+            title : string,
+            number: number,
+        }
+    }
+}
+
+export type GithubEvent =
+    "WatchEvent" | "PushEvent" | "PullRequestEvent" | "IssuesEvent";
---
+++ src/app/lib/types/GithubRepo.ts
@@ -0,0 +1,7 @@
+export interface GithubRepo {
+    name : string,
+    description : string,
+    starCount : number, 
+    language: string,
+    lastUpdated: string,
+}
