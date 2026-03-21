7ecff22bcb9e05210f07001d540955f4747d5ce9
Your Name <NRankapole@dvtsoftware.com>
2026-03-21 15:13:12 +0200

    feat: Add User interface

---
+++ src/app/lib/types/User.ts
@@ -0,0 +1,9 @@
+export interface User  {
+    username : string ,
+    name : string,
+    avatar : string,
+    bio : string | null,
+    followers : number,
+    following : number,
+    publicRepos : number,
+}
