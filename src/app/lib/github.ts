e50bcf3f2a28df9e60e38ed9926545b51fdc0faa
Your Name <NRankapole@dvtsoftware.com>
2026-03-22 16:45:26 +0200

    feat: Add function to get user event information

--- src/app/lib/github.ts
+++ src/app/lib/github.ts
@@ -1,5 +1,7 @@
 import { GithubUser } from "./types/GithubUser";
 import { GithubRepo, GithubRepoResponse } from "./types/GithubRepo";
+import { GithubEvent } from "./types/GithubEvent";
+import { isGithubEventResponse } from "./utils/githubEventValidator";
 
 
 
@@ -28,7 +30,7 @@ export async function getGithubUserRepos(username: string): Promise<GithubRepo[]
     const res = await fetch(`https://api.github.com/users/${username}/repos`);
 
     if (!res.ok) {
-        throw new Error("Failed to get  repos")
+        throw new Error("Failed to get user repos")
     }
 
     const data: GithubRepoResponse[] = await res.json();
@@ -39,13 +41,96 @@ export async function getGithubUserRepos(username: string): Promise<GithubRepo[]
             description: repo.description,
             starCount: repo.stargazers_count,
             language: repo.language,
-            lastUpdated: repo.updated_at,
+            lastUpdated: repo.updated_at
         }
     ))
-
 }
 
+export async function getGithubUserEvents(username: string): Promise<GithubEvent[]> {
+    const res = await fetch(`https://api.github.com/users/${username}/events/public`);
 
+    if (!res.ok) {
+        throw new Error("Failed to get user events");
+    }
 
+    const data = await res.json() as unknown[];
 
+    const events: GithubEvent[] = [];
 
+    for (const item of data) {
+        if (!isGithubEventResponse(item)) {
+            continue; //skip items that aren't valid event response
+        }
+
+        switch (item.type) {
+            case "WatchEvent":
+                events.push({
+                    id: item.id,
+                    type: "WatchEvent",
+                    createdAt: item.created_at,
+                    repo: {
+                        name: item.repo.name
+                    },
+                    payload: {
+                        action: item.payload.action
+                    }
+                });
+                break;
+
+            case "PushEvent":
+                events.push({
+                    id: item.id,
+                    type: "PushEvent",
+                    createdAt: item.created_at,
+                    repo: {
+                        name: item.repo.name
+                    },
+                    payload: {
+                        ref: item.payload.ref,
+                        commits: item.payload.commits.map((commit) => ({
+                            sha: commit.sha,
+                            message: commit.message
+                        }))
+                    }
+                });
+                break;
+
+            case "PullRequestEvent":
+                events.push({
+                    id: item.id,
+                    type: "PullRequestEvent",
+                    createdAt: item.created_at,
+                    repo: {
+                        name: item.repo.name,
+                    },
+                    payload: {
+                        action: item.payload.action,
+                        pullRequest: {
+                            title: item.payload.pull_request.title,
+                            htmlUrl: item.payload.pull_request.html_url
+                        },
+                    },
+                });
+                break;
+
+            case "IssuesEvent":
+                events.push({
+                    id: item.id,
+                    type: "IssuesEvent",
+                    createdAt: item.created_at,
+                    repo: {
+                        name: item.repo.name,
+                    },
+                    payload: {
+                        action: item.payload.action,
+                        issue: {
+                            title: item.payload.issue.title,
+                            number: item.payload.issue.number,
+                        },
+                    },
+                });
+                break;
+        }
+    }
+    return events;
+}
