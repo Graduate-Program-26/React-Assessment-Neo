425063794530ef58eb067aef848cd3ef759be5be
Your Name <NRankapole@dvtsoftware.com>
2026-03-22 14:44:18 +0200

    feat: Add event api response type

--- src/app/lib/types/GithubEvent.ts
+++ src/app/lib/types/GithubEvent.ts
@@ -10,7 +10,7 @@ interface GithubBaseEvent {
 interface WatchEvent extends GithubBaseEvent {
     type: "WatchEvent",
     payload: {
-        action: "started",
+        action: "started"
     }
 }
 
@@ -34,20 +34,77 @@ interface PullRequestEvent extends GithubBaseEvent {
         pullRequest: {
             title: string,
             htmlUrl: string,
-        };
+        }
     }
 }
 
 interface IssuesEvent extends GithubBaseEvent {
-    type : "IssuesEvent",
-    payload : {
-        action : "opened" | "closed" | "reopened" | "edited",
-        issue : {
-            title : string,
-            number: number,
+    type: "IssuesEvent",
+    payload: {
+        action: "opened" | "closed" | "reopened" | "edited",
+        issue: {
+            title: string,
+            number: number
         }
     }
 }
 
 export type GithubEvent =
     WatchEvent | PushEvent | PullRequestEvent | IssuesEvent;
+
+
+// Event API Response types
+interface GithubEventResponseBase {
+    id: string,
+    type: string,
+    created_at: string,
+    repo: {
+        name: string
+    }
+}
+
+interface WatchEventResponse extends GithubEventResponseBase {
+    type: "WatchEvent",
+    payload: {
+        action: "started"
+    }
+}
+
+interface CommitResponse {
+    sha: string,
+    message: string
+}
+
+interface PushEventResponse extends GithubEventResponseBase {
+    type: "PushEvent",
+    payload: {
+        ref: string,
+        commits: CommitResponse[]
+    }
+}
+
+
+interface PullRequestEventResponse extends GithubEventResponseBase {
+    type: "PullRequestEvent",
+    payload: {
+        action: "opened" | "closed" | "reopened",
+        pull_request: {
+            title: string,
+            html_url: string
+        }
+    }
+}
+
+interface IssuesEventResponse extends GithubEventResponseBase {
+    type: "IssuesEvent",
+    payload: {
+        action: "opened" | "closed" | "reopened" | "edited",
+        issue: {
+            title: string,
+            number: number
+        }
+    }
+}
+
+export type GithubEventResponse =
+    WatchEventResponse | PushEventResponse | PullRequestEventResponse | IssuesEventResponse;
