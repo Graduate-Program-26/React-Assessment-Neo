44d3db6119cc31f56bb07b4090f63012c2107093
Your Name <NRankapole@dvtsoftware.com>
2026-03-25 00:08:40 +0200

    feat: Add user search API function

---
+++ src/app/lib/types/GithubSearch.ts
@@ -0,0 +1,7 @@
+import { GithubUserResponse } from "./GithubUser"
+
+export interface GithubSearchResponse {
+    total_count: number
+    incomplete_results: boolean
+    items: GithubUserResponse[]
+}
